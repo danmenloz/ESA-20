ea03d61912de90c7ff6cb8f2bea547ff8fe17e63
Alex Dean <agdean@ncsu.edu>
2020-09-14 13:48:08 -0400

    Initial commit

---
+++ M1-Introduction/Arch_Intro_HBLED_Driver/Scripts/make_call_graph.bat
@@ -0,0 +1,9 @@
+@echo off
+REM This script is run by MDK, so it is run in <Project Directory>
+REM set PATH=%PATH;C:\Users\Alex\AppData\Local\Programs\Python\Python38
+set PATH=%PATH;..\..\..\Tools\Calls;"C:\Program Files (x86)\Graphviz2.38\bin"
+@echo on
+"..\..\Tools\Calls\call_graph_generator.py" -i mycallgraph.txt  -o cg.gv
+REM "C:\Users\Alex\Documents\Teaching\ESA\ESA-Fall-2020\Code\Keil-uVision-Call-Graph-Generator\call_graph_generator.py" -i mycallgraph.txt  -o cg.gv
+@echo off REM ..\..\..\Tools\Calls\call_graph_generator.py.lnk -i ..\mycallgraph.txt  -o ..\cg.gv
+dot -O -Tpdf cg.gv

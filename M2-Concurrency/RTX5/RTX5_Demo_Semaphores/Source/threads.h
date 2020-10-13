6dc94ccc7af3a05e50bf41141b27d8f23d1fb458
Alex Dean <agdean@ncsu.edu>
2020-10-13 10:29:16 -0400

    Updated to use letters, fast sequence.

--- M2-Concurrency/RTX5/RTX5_Demo_Semaphores/Source/threads.h
+++ M2-Concurrency/RTX5/RTX5_Demo_Semaphores/Source/threads.h
@@ -11,8 +11,8 @@ extern osSemaphoreId_t error_sem;
 void Thread_RGB(void * arg);
 void Thread_Error(void * arg);
 
-#define USE_COUNTING_SEM (0)
-#define USE_ERROR_HANDLING (0)
+#define USE_COUNTING_SEM (1)
+#define USE_ERROR_HANDLING (1)
 
 #define THREADS_H
 #endif // THREADS_H
